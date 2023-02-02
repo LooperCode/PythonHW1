7e5abcc8d866f02dc1ae49991af6992fd675b0af
LooperCode <savchenkodkz@gmail.com>
2023-02-03 02:25:58 +0500

    com

---
+++ HW1/main.py
@@ -0,0 +1,26 @@
+from decimal import Decimal
+a = Decimal(input('Введите число: '))
+temp = a
+if a < 0:
+    a = abs(a)
+s = a
+b = s * 10
+b %= 10
+b /= 10
+e = round(b, 10)
+while e != int(e):
+    e *= 10
+e = int(e)
+sumFloat = 0
+while e != 0:
+    sumFloat += e % 10
+    e //= 10
+a = int(a)
+while a != 0:
+    sumFloat += a % 10
+    a //= 10
+if temp < 0:    
+    print(-sumFloat)
+elif temp > 0:
+    print(sumFloat)
+    
